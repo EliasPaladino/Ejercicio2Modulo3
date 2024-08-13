67a9a11b01bed2ac7ae20896f2f36640ec96887e
epaladino <eliaspaladino@outlook.com>
2024-08-13 01:05:45 -0300

    Resolucion del Ejercicio2Modulo3

--- EjercicioModulo3Clase2/Controllers/TaskController.cs
+++ EjercicioModulo3Clase2/Controllers/TaskController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EjercicioModulo3Clase2.Domain.Entities;
+using EjercicioModulo3Clase2.Repository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EjercicioModulo3Clase2.Controllers
 {
-    [Route( "" )]
+    [Route( "tasks" )]
     [ApiController]
     public class TaskController : ControllerBase
     {
@@ -16,23 +18,64 @@ namespace EjercicioModulo3Clase2.Controllers
          */
         #endregion
 
+        private readonly ToDoListDBContext _context;
+
+        public TaskController( ToDoListDBContext context )
+        {
+            _context = context;
+        }
+
         #region Ejercicio 1
         // Crear un endpoint para obtener un listado de todas las tareas usando HTTP GET
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_context.Tasks.ToList());
+        }
         #endregion
 
         #region Ejercicio 2
         // Crear un endpoint para obtener una tarea por ID usando HTTP GET
+        [HttpGet("{id}")]
+        public IActionResult GetTaskById(int id)
+        {
+            return Ok(_context.Tasks.Where(w => w.Id == id).FirstOrDefault());
+        }
         #endregion
 
         #region Ejercicio 3
         // Crear un endpoint para crear una nueva tarea usando HTTP POST
+        [HttpPost]
+        public IActionResult CreateTask([FromBody] Tasks task)
+        {
+            _context.Add(task);
+            _context.SaveChanges();
+            return Ok();
+        }
         #endregion
 
         #region Ejercicio 4
         // Crear un endpoint para marcar una tarea como completada usando HTTP PUT
+        [HttpPut("{id}")]
+        public IActionResult UpdateTask([FromRoute] int id, [FromBody] Tasks task)
+        {
+            var t = _context.Tasks.FirstOrDefault(w => w.Id == id);
+            t.IsCompleted = task.IsCompleted;
+            _context.SaveChanges();
+            return Ok(t);
+        }
         #endregion
         #region Ejercicio 5
         // Crear un endpoint para dar de baja una tarea usando HTTP PUT (baja lógica)
+        [HttpPut("eliminar/{id}")]
+        public IActionResult DeleteTaskById([FromRoute] int id)
+        {
+            var t = _context.Tasks.FirstOrDefault(x => x.Id == id);
+            t.IsActive = false;
+            _context.SaveChanges();
+            return Ok(t);
+        }
         #endregion
     }
 }
